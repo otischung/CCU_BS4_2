18a0bb1d032dfc1593e24922c8304e85514849c9
otischung <my072814638@gmail.com>
2022-02-23 11:30:59 +0800

    Add debug information

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, parse_qs
 import data
+import os
 import time
 
 session = requests.session()
@@ -13,16 +14,31 @@ fake_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.3
 
 
 def login():
-    login_r = session.post("https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/bookmark.php", data=data.login_data, headers=fake_headers)
+    login_r = session.post("https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/bookmark.php", data=data.login_data,
+                           headers=fake_headers)
 
-    # print(login_r.history)  # 302
-    # print(login_r.url)  # The URL contents session ID.
+    if os.getenv("DEBUG"):
+        print("login_r.history: " + str(login_r.history))  # Should be 302.
+        print("login_r.url:")  # The URL contents session ID.
+        print(login_r.url)
 
     # Reference: https://stackoverflow.com/questions/5074803/retrieving-parameters-from-a-url
     if not login_r.history:
         print("Login failed: Please check your account or your password.")
         quit(1)
+
     ssid = parse_qs(urlparse(login_r.url).query)["session_id"][0]
+    if os.getenv("DEBUG"):
+        print("login_r.url: " + login_r.url)
+        print("urlparse: " + str(urlparse(login_r.url)))
+        print("query: " + str(urlparse(login_r.url).query))
+        print("parse_qs: " + str(parse_qs(urlparse(login_r.url).query)))
+        print("ssid: " + ssid)
+
+    login_r.encoding = "utf-8"
+    user = str(BeautifulSoup(login_r.text, "html.parser").select("TD")[0].get_text()).split(' ')[0]
+    if os.getenv("DEBUG"):
+        print("Login as " + user)
     return ssid
 
 
@@ -32,8 +48,6 @@ def choose(ssid):
 
     data.choose["session_id"] = ssid
     c2 = session.post("https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/Add_Course01.cgi", data=data.choose, headers=fake_headers)
-    # print(c2.content.decode("utf-8"))
-    # input()
     return c2.content.decode("utf-8")
 
 
@@ -44,6 +58,11 @@ def check(choose_content):
         print("already selected!")
         exit(0)
     chk_box = chk_box[0]
+    if os.getenv("DEBUG"):
+        print("Course Information:")
+        print("Course: " + chk_box.parent.parent.select("th")[3].get_text())
+        print("Class: " + chk_box.parent.parent.select("th")[5].get_text())
+        print("Professor: " + chk_box.parent.parent.select("th")[4].get_text())
     l = int(chk_box.parent.parent.select("th")[2].get_text())  # left
     return l
 
@@ -51,7 +70,7 @@ def check(choose_content):
 def select(ssid):
     data.select["session_id"] = ssid
     r = session.post("https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/Add_Course01.cgi", data=data.select, headers=fake_headers)
-    print(r.content.decode("utf-8"))
+    # print(r.content.decode("utf-8"))
     print(f"\nTry {try_times} times.")
     print("Success.")
     return True
